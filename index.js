99e39501f55c645adc67f74af46cba005de41262
zemelatt <zemelatmen@gmail.com>
2023-04-19 13:17:23 -0700

    firstmmmlml

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-const db = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});
-module.exports = db;
-db.connect((err) => {
-  if (err) console.log(err);
-  console.log("db connected");
-});
+// const db = mysql.createConnection({
+//   host: process.env.MYSQL_HOST,
+//   user: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+// });
+// db.connect((err) => {
+//   if (err) console.log(err);
+//   console.log("db connected");
+// });
 
 app.get("/", (req, res) => {
   res.send({ title: "Books" });
 });
-app.listen(port);
+app.listen(port, () => {
+  console.log("started");
+});
