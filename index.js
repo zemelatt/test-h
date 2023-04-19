5f2e33a09882bc0a4a2e0af6f33eb61833c501f1
zemelatt <zemelatmen@gmail.com>
2023-04-19 13:08:26 -0700

    firstmmmlml

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mysql = require("mysql");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.use(express.json());
 const db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
