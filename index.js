0f766778dc925aff8afca8ebfc38f0a87d9ca44f
zemelatt <zemelatmen@gmail.com>
2023-04-19 13:05:29 -0700

    firstmmmlm

--- index.js
+++ index.js
@@ -6,11 +6,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  port: "3306",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 module.exports = db;
 db.connect((err) => {
