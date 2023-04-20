eb67d667394fcce0ad22be7ee090d2d14cc00207
zemelatt <zemelatmen@gmail.com>
2023-04-20 05:39:55 -0700

    fport

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ let db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
+  port: process.env.MYSQL_PORT,
 });
 db.connect((err) => {
   if (err) console.log(err);
