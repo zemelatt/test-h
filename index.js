af64327a15a2fa8b7cb81ea9284703235eb3fe2e
zemelatt <zemelatmen@gmail.com>
2023-04-20 02:50:29 -0700

    fnil

--- index.js
+++ index.js
@@ -5,44 +5,65 @@ const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-const db = mysql.createConnection({
+let db = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
-  // database: process.env.MYSQL_DATABASE,
 });
 db.connect((err) => {
-  if (err) throw err;
-  console.log("Connected!");
-  db.query("CREATE DATABASE IF NOT EXISTS Mydb", (err, result) => {
-    if (err) throw err;
-    console.log("db created");
+  if (err) console.log(err);
+  db.query("CREATE DATABASE IF NOT EXISTS mydb", function (err, result) {
+    if (err) {
+      console.log(err);
+    } else {
+      db.changeUser({ database: process.env.MYSQL_DATABASE }, (err) => {
+        if (err) throw err;
+      });
+      console.log("Database created");
+    }
   });
 });
 
-//
+// db.changeUser(
+//   {
+//     database: process.env.Mydb,
+//   },
+//   (err) => {
+//     if (err) {
+//       console.log("Error in changing database", err);
+//       return;
+//     }
+//   }
+// );
 // app.use(express.static(path.join(__dirname, "")));
 // app.get('*', (req, res) => {
 //   res.sendFile(path.join(__dirname,'./clint/build/index.html'))
 // })
+var select = "insert into customers(name, address) values('zemelat','admin')";
 var createSql =
   "CREATE TABLE IF NOT EXISTS customers (name VARCHAR(255), address VARCHAR(255))";
 var All = "select * from customers";
-app.get("/all", (req, res) => {
-  db.query(All, function (err, result) {
-    if (err) throw err;
-    res.send(result);
-  });
-  // res.send({ title: "Books" });
-});
-var select = "insert into customers(name, address) values('zemelat','admin')";
 app.get("/", (req, res) => {
-  db.query(select, function (err, result) {
+  db.query(createSql, function (err, result) {
     if (err) throw err;
-    res.send(result);
+    db.query(select, function (err, result) {
+      if (err) throw err;
+      res.send(result);
+    });
   });
   // res.send({ title: "Books" });
 });
+
+app.get("/all", (req, res) => {
+  db.query(createSql, function (err, result) {
+    if (err) throw err;
+    db.query(All, function (err, result) {
+      if (err) throw err;
+      res.send(result);
+    });
+  });
+});
+
 app.listen(port, () => {
   console.log("started");
 });
