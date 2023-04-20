e066d846cafa3dac12ec75d30fbfdab58966d8fa
zemelatt <zemelatmen@gmail.com>
2023-04-19 23:30:09 -0700

    firstmmmlmlh

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ const mysql = require("mysql");
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-// const db = mysql.createConnection({
-//   host: process.env.MYSQL_HOST,
-//   user: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE,
-// });
-// db.connect((err) => {
-//   if (err) console.log(err);
-//   console.log("db connected");
-// });
+const db = mysql.createConnection({
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+});
+db.connect((err) => {
+  if (err) console.log(err);
+  console.log("db connected");
+});
+
+// app.use(express.static(path.join(__dirname,'')))
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname,'./clint/build/index.html'))
+// })
 
 app.get("/", (req, res) => {
   res.send({ title: "Books" });
